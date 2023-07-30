ff7bc53329e16f0242ad411923021c08230463aa
Kelsie Marklin <kelsiethefirst@gmail.com>
2023-07-30 02:27:07 -0700

    groups working

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import (Flask, render_template, request, session, redirect)
 from jinja2 import StrictUndefined
-from model import db, connect_to_db, User, Group
+from model import db, connect_to_db, User, Group, UserGroup
 from datetime import datetime
 from sqlalchemy import and_
 import crud
@@ -16,11 +16,16 @@ def homepage():
     return render_template('index.html')
 
 
-@app.route('/create-user', methods=['GET', 'POST'])
+@app.route('/users', methods=['POST'])
 def create_user():
-    name = request.form.get('first-name')
-    username = request.form.get('username')
-    password = request.form.get('password')
+    username = request.form.get('new-username')
+    name = request.form.get('new-name')
+    password = request.form.get('new-password')
+
+    existing_username = User.query.filter_by(username=username).first()
+    
+    if existing_username:
+        return {'success': False, 'message': 'Username unavailable'}
 
     user = crud.create_user(username, password, name)
     db.session.add(user)
@@ -47,35 +52,57 @@ def login():
     return {'success': False, 'message': 'Incorrect password'}
 
 
-@app.route('/groups', methods=['POST'])
+@app.route('/create-group', methods=['POST'])
 def create_group():
     """creates a new group and assigns the user to it"""
+    username = session['username']
+    user = User.query.filter_by(username=username).first()
+
     name = request.form.get('new-group-name')
     password = request.form.get('new-group-password')
 
     group = crud.create_group(password=password, name=name)
-
     db.session.add(group)
     db.session.commit()
 
+    group_in_db = Group.query.filter_by(name=name).first()
+    user_group = crud.create_user_group(user_id=user.user_id, group_id=group_in_db.group_id)
 
-@app.route('/groups', methods=['PATCH'])
+    db.session.add(user_group)
+    db.session.commit()
+
+
+    return {'success': True, 'message': f'Group {name} created'}
+
+
+@app.route('/join-group', methods=['POST'])
 def join_group():
     name = request.form.get('group-name')
     password = request.form.get('group-password')
     
     username = session['username']
-    cur_user = User.query.filter_by(username=username).first()
+    user = User.query.filter_by(username=username).first()
 
     group = Group.query.filter_by(name=name).first()
+    print(name, password, user, group, 'line 87')
     
     if not group:
+        print('if not group')
         return {'success': False, 'message': 'Group not found'}
     
     if group.check_password(password):
-        cur_user.group_id = group.group_id
-        return {'success': True, 'message': 'Added to group {name}'}
-    
+        print('if group.check_password')
+        existing_user_group = UserGroup.query.filter_by(user_id=user.user_id, group_id=group.group_id).first()
+        print(existing_user_group, 'existing_user_group')
+        if existing_user_group:
+            return {'success': False, 'message': f"You are already in group '{name}'"}
+        
+        user_group = crud.create_user_group(user_id=user.user_id, group_id=group.group_id)
+        db.session.add(user_group)
+        db.session.commit()
+
+        return {'success': True, 'message': f'Added to group {name}'}
+
     return {'success': False, 'message': 'Incorrect password'}
 
 
